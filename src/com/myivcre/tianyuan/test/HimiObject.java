85e241ad6bd11c8f8c80749c8dbf057368772bfa
freepander <freepander@163.com>
2014-04-09 21:33:54 +0800

    增加了鸿雁传书机构

---
+++ src/com/myivcre/tianyuan/test/HimiObject.java
@@ -0,0 +1,25 @@
+package com.myivcre.tianyuan.test;
+
+public class HimiObject {
+
+	private int id;
+	private String name;
+	public HimiObject(int id,String name){
+		this.id=id;
+		this.name=name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
